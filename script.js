aae95dcd7dae2cea610fe5462569bc4fe677b61c
BerettaSM <ramonsaviato@hotmail.com>
2023-07-14 01:19:12 -0300

    feat: Add redimension and clear capabilities

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const redimensionBtn = document.getElementById('redimension');
+const clearBtn = document.getElementById('clear')
 
-container.addEventListener('mouseover', e => {
-    if(e.target === e.currentTarget) return
+const MAX_SIDE_SIZE = 100
 
-    const square = e.target
-    square.classList.add('painted')
-})
+createDivs(16 ** 2);
 
-createDivs()
+container.addEventListener('mouseover', paintSquare);
+redimensionBtn.addEventListener('click', askForNewDimensions)
+clearBtn.addEventListener('click', clear)
 
-function createDivs() {
-    const quantity = 16 * 16
+
+function createDivs(quantity) {
+    container.innerHTML = ''
 
     for (let i = 0; i < quantity; i++) {
-        const div = document.createElement('div')
-        div.className = 'square'
-        container.appendChild(div)
+        const div = document.createElement('div');
+        div.className = 'square';
+        container.appendChild(div);
     }
+
+}
+
+function paintSquare(event) {
+    // Assure its a square, and not the container itself.
+    if (event.target === event.currentTarget) return;
+
+    const square = event.target;
+    square.classList.add('painted');
+}
+
+function askForNewDimensions() {
+    const input = window.prompt('Enter the number of squares per side for the new grid: ', '')
+
+    if(!input) return
+
+    const numSides = +input
+
+    if(isNaN(numSides) || numSides < 1 || numSides > MAX_SIDE_SIZE) {
+        alert(`Invalid input. The number of sides must be between 1 and ${MAX_SIDE_SIZE}!`)
+        return
+    }
+
+    resizeGrid(numSides)
+}
+
+function resizeGrid(numSides) {
+    const gridStyle = `repeat(${numSides}, 1fr) / repeat(${numSides}, 1fr)`;
+    container.style.grid = gridStyle
+
+    createDivs(numSides ** 2)
+}
+
+function clear() {
+    for(const square of container.children)
+        square.classList.remove('painted')
 }
